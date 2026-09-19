ff2b569a9443ce8b945af96af9347837a9007440
Archil Marukashvili <achiko2002@hotmail.com>
2026-09-19 12:10:14 +0100

    Added 2 branches, feat and fix

--- Assets/Scripts/MobileBootstrap.cs
+++ Assets/Scripts/MobileBootstrap.cs
@@ -4,6 +4,8 @@ public class MobileBootstrap : MonoBehaviour
 {
     void Awake()
     {
+        //Comment
+
         // Android defaults to 30 fps when targetFrameRate is -1.
         Application.targetFrameRate = 60;
         // vSyncCount is ignored on Android; keep it at 0.
